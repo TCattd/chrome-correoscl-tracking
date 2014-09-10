4e93ff26f2c85d2f3208e739d7ae63a19b44a63c
Esteban <esteban.git@attitude.cl>
2014-09-10 12:36:49 -0300

    Fix al cambio de eBay, de nuevo. Probando tracking en adidas.cl

--- scripts/trackingfacil.js
+++ scripts/trackingfacil.js
@@ -11,8 +11,7 @@ jQuery( document ).ready(function( $ ) {
 
     //eBay
     if(currentDomain.indexOf('ebay.com') > -1) {
-        //Tracking number en el historial de compras
-        //.tracking-label>a.iframe-modal
+        //Tracking number en el historial de compras v2
         if(currentPath.indexOf('eBayISAPI') > -1) {
             var trackingLoopPurchaseHistory = setInterval(function(){
                 $("div[id^='track_']").each(function() {
@@ -24,6 +23,23 @@ jQuery( document ).ready(function( $ ) {
                     //Si el número de tracking que encontramos a continuación no es el trackign de este loop, entonces creamos el link
                     if(trackingNext != trackingNumber && trackingNumber != '--') {
                         $(this).after(' <div class="seguimientowrap" data-tracking="'+trackingNumber+'"><a href="http://www.correos.cl/SitePages/seguimiento/seguimiento.aspx?envio='+trackingNumber+'" class="seguimientoCorreos" id="seguimientoCorreos_'+trackingNumber+'" data-tracking="'+trackingNumber+'" target="_blank">Seguimiento en Correos.cl</a></div>');
+                    }
+                });
+            }, 1000);
+        }
+
+        //Tracking en el historial de compras v1
+        //.tracking-label>a.iframe-modal
+        if(currentPath.indexOf('PurchaseHistory') > -1) {
+            var trackingLoopPurchaseHistory = setInterval(function(){
+                $('.tracking-label>a[title="Tracking number"]').each(function(){
+                    var trackingNumber = $(this).text();
+                    var trackingNext   = $(this).next('.seguimientoCorreos').attr('data-tracking');
+
+                    //if(!$('#seguimientoCorreos_'+trackingNumber).length) { //Dos envios con el mismo número, evitan que se cree uno nuevo al lado de cada número de tracking con esta condición
+                    //Si el número de tracking que encontramos a continuación no es el trackign de este loop, entonces creamos el link
+                    if(trackingNext != trackingNumber) {
+                        $(this).after(' // <a href="http://www.correos.cl/SitePages/seguimiento/seguimiento.aspx?envio='+trackingNumber+'" class="seguimientoCorreos" id="seguimientoCorreos_'+trackingNumber+'" data-tracking="'+trackingNumber+'" target="_blank">Seguimiento en Correos.cl</a>');
                     }
                 });
             }, 1000);
@@ -68,6 +84,28 @@ jQuery( document ).ready(function( $ ) {
             if(sentThrough.indexOf('fedex') > -1) {
                 $('.shipping-bd>.no').html(trackingNumber+'<br/><a href="http://www.fedex.com/fedextrack/html/index.html?tracknumbers='+trackingNumber+'" class="seguimientoCorreos" id="seguimientoCorreos_'+trackingNumber+'" data-tracking="'+trackingNumber+'" target="_blank">Seguimiento en FedEx.com</a>');
             }
+        }
+    }
+
+    //Adidas.cl
+    if(currentDomain.indexOf('adidas.cl') > -1) {
+        console.log('en Adidas');
+        //Tracking en el historial de compras
+        if(currentPath.indexOf('Order-Histor') > -1) {
+            console.log('en el Historial');
+            var trackingAdidasPurchaseHistory = setInterval(function(){
+                $('.ordernum').each(function(){
+                    var trackingNumber = $(this).text();
+                    console.log(trackingNumber);
+                    var trackingNext   = $(this).next('.seguimientoChilexp').attr('data-tracking');
+                    console.log(trackingNext);
+
+                    //Si el número de tracking que encontramos a continuación no es el trackign de este loop, entonces creamos el link
+                    if(trackingNext != trackingNumber && trackingNumber != '') {
+                        $(this).after('<br/><a href="https://www.chilexpress.cl/Views/ChilexpressCL/Resultado-busqueda.aspx?DATA='+trackingNumber+'" class="seguimientoChilexp" id="seguimientoChilexp_'+trackingNumber+'" data-tracking="'+trackingNumber+'" target="_blank">Seguimiento Chilexpress.cl</a>');
+                    }
+                });
+            }, 1000);
         }
     }
 });
